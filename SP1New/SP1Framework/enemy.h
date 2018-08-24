e1cb67d58d84a4a0381343dc9e07e98574418044
Herophillix <42328650+Herophillix@users.noreply.github.com>
2018-08-24 15:21:30 +0800

    Add files via upload

---
+++ SP1New/SP1Framework/enemy.h
@@ -0,0 +1,31 @@
+#ifndef _ENEMY_H
+#define _ENEMY_H
+
+extern int timer;
+extern int timer1;
+extern int timer2;
+extern int Direction;
+extern int Direction3;
+extern int Direction4;
+extern int Direction5;
+extern int Direction6;
+extern int one;
+extern int two;
+
+extern int enemylocationX;
+extern int enemylocationY;
+extern int enemy1locationX;
+extern int enemy1locationY;
+extern int enemy2locationX;
+extern int enemy2locationY;
+extern int enemy3locationX;
+extern int enemy3locationY;
+extern int enemy4locationX;
+extern int enemy4locationY;
+extern int enemy5locationX;
+extern int enemy5locationY;
+extern int enemy6locationX;
+extern int enemy6locationY;
+
+#endif // !1
+
