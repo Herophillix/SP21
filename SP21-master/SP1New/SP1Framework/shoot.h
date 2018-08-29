e08e1fe0c58d6933e2ccae30bb78865bfc5af622
eugeneyj <42328890+eugeneyj@users.noreply.github.com>
2018-08-29 09:27:37 +0800

    Add files via upload

---
+++ SP21-master/SP1New/SP1Framework/shoot.h
@@ -0,0 +1,18 @@
+#ifndef _SHOOT_H
+#define _SHOOT_H
+
+using namespace std;
+
+void getInputshoot();
+extern int bulletcondition;
+extern int ShootDirection;
+extern bool Bulletpos;
+extern bool BulletposPRed;
+extern bool BulletposPBlue;
+
+extern int ShootDirectionFinal;
+extern int ShootDirectionFinalRed;
+extern int ShootDirectionFinalBlue;
+extern int ShootDirectionEnemy;
+
+#endif
